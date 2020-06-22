eb0a37096aebab2940ef26c9b9f0d39925c7a2c3
ismael_PR <34767708+smgl9@users.noreply.github.com>
2020-06-22 13:35:09 +0200

    add model and float test

---
+++ tb/functions.py
@@ -0,0 +1,4 @@
+
+def gamma_f(gamma,v_in):
+    v_out=v_in**gamma
+    return v_out
--- tb/gamma_tb.py
+++ tb/gamma_tb.py
@@ -3,6 +3,7 @@ import cocotb
 from cocotb.triggers import Timer
 from cocotb.result import TestFailure
 import random
+from functions import gamma_f
 
 @cocotb.coroutine
 def gen_clk(clk, period):
@@ -14,22 +15,28 @@ def gen_clk(clk, period):
 
 @cocotb.test()
 def gamma_testAlive(dut):
+    c_gamma=0.45
     Period = 10
     clk=dut.clk
     cocotb.fork(gen_clk(clk, Period))
     yield Timer(20*Period)
     reset = 0
-    dv_in = random.randint(0, 1)
-    gamma_in = random.randint(0, 1)
-    data_in = random.randint(0, 1)
-    dut.reset = reset
-    dut.dv_in = dv_in
-    dut.gamma_in = gamma_in
-    dut.data_in = data_in
-    yield Timer(20*Period)
-    print(dut.data_out)
-    if int(dut.data_out) != int(dut.data_out):
-        raise TestFailure(
-        "result is incorrect: %s != %s" % str(dut.data_out))
-    else:
-        dut._log.info("Ok!")
+    for i in range(0,10):
+        dv_in = random.randint(0, 1)
+        gamma_in = random.randint(0, 1)
+        data_in = random.randint(0, 100)
+        dut.reset = reset
+        dut.dv_in = dv_in
+        dut.gamma_in = gamma_in
+        dut.data_in = data_in
+        yield Timer(20*Period)
+        print(dut.data_out)
+        print(data_in)
+        print(gamma_f(c_gamma,data_in))
+        print(abs(int(dut.data_out) - int(gamma_f(c_gamma,data_in))))
+        if abs(int(dut.data_out) - int(gamma_f(c_gamma,data_in)))>1:
+            raise TestFailure(
+            "result is incorrect: %s != %s" % (str(int(dut.data_out)), int(gamma_f(c_gamma,data_in))))
+        else:
+            dut._log.info("Ok!")
+
