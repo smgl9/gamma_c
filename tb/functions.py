a1749c1a7a6cf5f366cfea1223195249dc9a9507
ismael_PR <34767708+smgl9@users.noreply.github.com>
2020-06-22 18:47:37 +0200

    fixed point output

--- tb/functions.py
+++ tb/functions.py
@@ -4,7 +4,6 @@ def gamma_f(gamma,v_in):
     v_out=v_in**gamma
     return v_out
 
-def gamma(gamma,v_in):
-    
-    v_out=v_in**gamma
-    return v_out
+def gamma_fp(gamma):
+    gamma_fp_value = FpBinary(int_bits=8, frac_bits=0, signed=False, value=gamma)
+    return gamma_fp_value
--- tb/gamma_tb.py
+++ tb/gamma_tb.py
@@ -3,7 +3,7 @@ import cocotb
 from cocotb.triggers import Timer
 from cocotb.result import TestFailure
 import random
-from functions import gamma_f, gamma
+from functions import gamma_f, gamma_fp
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -30,33 +30,45 @@ def gamma_testAlive(dut):
     A_scan = 2**8/(2**12)**c_gamma 
     A_disp = 2**8/(2**12)**(1/c_gamma) 
     # normalized data input (0-1)
-    in_data_f = np.linspace(0, 1, num=2**12)
+    in_data_norm = np.linspace(0, 1, num=2**12)
     vect_gamma_f = np.vectorize(gamma_f)
     out_data = vect_gamma_f(c_gamma,in_data)
-    out_data_fp = out_data*A_scan
-    out_data_f = vect_gamma_f(c_gamma,in_data_f)
+    out_data_f = out_data*A_scan
+    out_data_fnorm = vect_gamma_f(c_gamma,in_data_norm)
     out_data_disp = vect_gamma_f(1/c_gamma,in_data)
-    out_data_disp_fp =out_data_disp*A_disp
-    out_data_disp_f = vect_gamma_f(1/c_gamma,in_data_f)
+    out_data_disp_f =out_data_disp*A_disp
+    out_data_disp_fnorm = vect_gamma_f(1/c_gamma,in_data_norm)
+    
+    # module out model
+    vect_gamma_fp = np.vectorize(gamma_fp)
+    # out_data_fp = vect_gamma_fp(out_data_f)
+    out_data_fp = np.arange(0, 2**12, 1)
+    for i in range(0,2**12):
+        out_data_fp[i] = gamma_fp(out_data_f[i])
     # plot
+    # plt.plot(out_data_fnorm)
+    # plt.plot(out_data_disp_fnorm)
+    # plt.show()
+    # plt.plot(out_data_disp)
+    # plt.show()
+    # plt.plot(out_data)
+    # plt.show()
     plt.plot(out_data_f)
     plt.plot(out_data_disp_f)
-    plt.show()
-    plt.plot(out_data_disp)
-    plt.show()
-    plt.plot(out_data)
-    plt.show()
     plt.plot(out_data_fp)
-    plt.plot(out_data_disp_fp)
     plt.show()
-    print(in_data)
-    print(in_data)
-    print(out_data)
+    plt.plot(abs(out_data_f-out_data_fp))
+    plt.show()
+    print(in_data.size)
+    print(in_data.size)
+    print(out_data.size)
+    print(out_data_fp)
+
     # Random test
     for i in range(0,10):
         dv_in = random.randint(0, 1)
         gamma_in = random.randint(0, 1)
-        data_in = random.randint(0, 100)
+        data_in = random.randint(0, 2**12-1)
         dut.reset = reset
         dut.dv_in = dv_in
         dut.gamma_in = gamma_in
@@ -66,7 +78,7 @@ def gamma_testAlive(dut):
         print(data_in)
         print(gamma_f(c_gamma,data_in))
         print(abs(int(dut.data_out) - int(gamma_f(c_gamma,data_in))))
-        if abs(int(dut.data_out) - int(gamma_f(c_gamma,data_in)))>1:
+        if abs(int(dut.data_out) - out_data_fp[data_in])>1:
             raise TestFailure(
             "result is incorrect: %s != %s" % (str(int(dut.data_out)), int(gamma_f(c_gamma,data_in))))
         else:
