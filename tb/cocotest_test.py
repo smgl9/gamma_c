91c985d9ded7173e3dfa7c11e34b004b3f0a4539
ismael_PR <34767708+smgl9@users.noreply.github.com>
2020-06-23 11:22:03 +0200

    revert

--- tb/cocotest_test.py
+++ tb/cocotest_test.py
@@ -2,10 +2,10 @@ from cocotb_test.run import run
 import pytest
 import os
 
-def test_adder_vhdl():
+def gamma_test():
     run(vhdl_sources=["../src/gamma.vhd"],
         simulation_args=["--vcd=func.vcd"],
         toplevel="gamma",
-        module="cocotb",
+        module="gamma_testAlive",
         toplevel_lang="vhdl"
     )
