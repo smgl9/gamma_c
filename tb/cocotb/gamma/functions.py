b3fc5fe4b4e919d047f999efb9e4cdcb84cf6432
ismael <ismael@ismael-asus>
2022-05-15 23:51:50 +0200

    - update fixed point library

--- tb/cocotb/gamma/functions.py
+++ tb/cocotb/gamma/functions.py
@@ -1,12 +1,13 @@
-from fpbinary import FpBinary, OverflowEnum, RoundingEnum
+from fxpmath import Fxp
 import numpy as np
+import matplotlib.pyplot as plt
 
 def gamma_f(gamma,v_in):
     v_out=v_in**gamma
     return v_out
 
 def gamma_fp(gamma):
-    gamma_fp_value = FpBinary(int_bits=8, frac_bits=0, signed=False, value=gamma)
+    gamma_fp_value = Fxp(gamma, signed=False, n_word=8, n_frac=0 ) # dtype='U8.0'
     return gamma_fp_value
 
 def data_gen():
@@ -25,7 +26,11 @@ def data_gen():
     out_data_disp = vect_gamma_f(1/c_gamma,in_data)
     out_data_disp_f =out_data_disp*A_disp
     out_data_disp_fnorm = vect_gamma_f(1/c_gamma,in_data_norm)
-        # plot
+    # module out model
+    out_data_fp = np.arange(0, 2**12, 1)
+    for i in range(0,2**12):
+        out_data_fp[i] = gamma_fp(out_data_f[i])
+    ##### plot
     # plt.plot(out_data_fnorm)
     # plt.plot(out_data_disp_fnorm)
     # plt.show()
@@ -44,8 +49,4 @@ def data_gen():
     # print(out_data.size)
     # print(out_data_fp)
     
-    # module out model
-    out_data_fp = np.arange(0, 2**12, 1)
-    for i in range(0,2**12):
-        out_data_fp[i] = gamma_fp(out_data_f[i])
     return out_data_fp
